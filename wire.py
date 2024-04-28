05ef89c4ef9a2b83e5669f074ee373631e08ac86
Cassius Ali Garcia <cassiusgarcia1@gmail.com>
2024-04-28 18:39:35 -0400

    Seperated wire class, added a file parser

---
+++ wire.py
@@ -0,0 +1,8 @@
+class Wire:
+    def __init__(self, line):
+        self.start_wire = line[0]
+        self.stop_wire = line[1]
+        self.a0 = line[2]
+        self.a1 = line[3]
+        self.a2 = line[4]
+        self.a3 = line[5]
